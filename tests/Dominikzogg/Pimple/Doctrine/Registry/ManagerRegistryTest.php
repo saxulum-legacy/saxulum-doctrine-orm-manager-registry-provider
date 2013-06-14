ed79db5408c17d445ad50131eced5629ee5daf89
Dominik Zogg <dominik.zogg@gmail.com>
2013-06-14 23:47:13 +0200

    add first test

---
+++ tests/Dominikzogg/Pimple/Doctrine/Registry/ManagerRegistryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Dominikzogg\Pimple\Doctrine\Registry;
+
+use Doctrine\ORM\EntityManager;
+use Dominikzogg\Pimple\Provider\DoctrineOrmManagerRegistryProvider;
+
+class ManagerRegistryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return array
+     */
+    protected function createMockDefaultAppAndDeps()
+    {
+        $app = new \Pimple;
+
+        $connection = $this
+            ->getMockBuilder('Doctrine\DBAL\Connection')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $app['dbs'] = new \Pimple(array(
+            'default' => $connection,
+        ));
+
+        $app['dbs.default'] = 'default';
+
+        $configuration = $this->getMock('Doctrine\ORM\Configuration');
+
+        $configuration
+            ->expects($this->any())
+            ->method('getEntityNamespace')
+            ->will($this->returnValue('Dominikzogg\Pimple\Doctrine\Registry\ManagerRegistryTest'))
+        ;
+
+        $repository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
+
+        $metadataFactory = $this->getMock('Doctrine\ORM\Mapping\ClassMetadataFactory');
+
+        $entityManager = $this
+            ->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $entityManager
+            ->expects($this->any())
+            ->method('getConfiguration')
+            ->will($this->returnValue($configuration))
+        ;
+
+        $entityManager
+            ->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($repository))
+        ;
+
+        $entityManager
+            ->expects($this->any())
+            ->method('getMetadataFactory')
+            ->will($this->returnValue($metadataFactory))
+        ;
+
+        $app['orm.ems'] = new \Pimple(array(
+            'default' => $entityManager,
+        ));
+
+        $app['orm.ems.default'] = 'default';
+
+        return $app;
+    }
+
+    public function testRegisterDefaultImplementations()
+    {
+        $app = $this->createMockDefaultAppAndDeps();
+
+        $doctrineOrmManagerRegistryProvider = new DoctrineOrmManagerRegistryProvider();
+        $doctrineOrmManagerRegistryProvider->register($app);
+
+        $this->assertEquals($app['doctrine']->getDefaultConnectionName(), 'default');
+        $this->assertInstanceOf('Doctrine\DBAL\Connection', $app['doctrine']->getConnection());
+        $this->assertInstanceOf('\Pimple', $app['doctrine']->getConnections());
+        $this->assertCount(1, $app['doctrine']->getConnectionNames());
+
+        $this->assertEquals($app['doctrine']->getDefaultManagerName(), 'default');
+        $this->assertInstanceOf('Doctrine\ORM\EntityManager', $app['doctrine']->getManager());
+        $this->assertInstanceOf('\Pimple', $app['doctrine']->getManagers());
+        $this->assertCount(1, $app['doctrine']->getManagerNames());
+        $this->assertEquals($app['doctrine']->getAliasNamespace('Test'), 'Dominikzogg\Pimple\Doctrine\Registry\ManagerRegistryTest');
+        $this->assertInstanceOf('Doctrine\Common\Persistence\ObjectRepository', $app['doctrine']->getRepository('Dominikzogg\Pimple\Doctrine\Registry\ManagerRegistryTest'));
+        $this->assertInstanceOf('Doctrine\ORM\EntityManager', $app['doctrine']->getManagerForClass('Dominikzogg\Pimple\Doctrine\Registry\ManagerRegistryTest'));
+    }
+}
