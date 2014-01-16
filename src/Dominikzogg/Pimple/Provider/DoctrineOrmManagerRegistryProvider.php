04759cb25099d2634ddf16611424bd08cd6d7c07
Dominik Zogg <dominik.zogg@gmail.com>
2014-01-16 01:12:20 +0100

    add orm commands

--- src/Dominikzogg/Pimple/Provider/DoctrineOrmManagerRegistryProvider.php
+++ src/Dominikzogg/Pimple/Provider/DoctrineOrmManagerRegistryProvider.php
@@ -3,6 +3,20 @@
 namespace Dominikzogg\Pimple\Provider;
 
 use Dominikzogg\Doctrine\Registry\ManagerRegistry;
+use Saxulum\DoctrineOrmCommands\Command\CreateDatabaseDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\DropDatabaseDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\ClearMetadataCacheDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\ClearQueryCacheDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\ClearResultCacheDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\ConvertMappingDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\CreateSchemaDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\DropSchemaDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\EnsureProductionSettingsDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\InfoDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\RunDqlDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\RunSqlDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\UpdateSchemaDoctrineCommand;
+use Saxulum\DoctrineOrmCommands\Command\Proxy\ValidateSchemaCommand;
 use Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension;
 
 class DoctrineOrmManagerRegistryProvider
@@ -13,12 +27,35 @@ class DoctrineOrmManagerRegistryProvider
             return new ManagerRegistry($container);
         });
 
-        if (class_exists('Symfony\\Bridge\\Doctrine\\Form\\DoctrineOrmExtension')) {
+        if (isset($container['form.extensions']) &&class_exists('Symfony\\Bridge\\Doctrine\\Form\\DoctrineOrmExtension')) {
             $container['form.extensions'] = $container->share(
-                $container->extend('form.extensions', function ($extensions, $app) {
-                    $extensions[] = new DoctrineOrmExtension($app['doctrine']);
+                $container->extend('form.extensions', function ($extensions, $container) {
+                    $extensions[] = new DoctrineOrmExtension($container['doctrine']);
 
                     return $extensions;
+                })
+            );
+        }
+
+        if (isset($container['console.commands'])) {
+            $container['console.commands'] = $container->share(
+                $container->extend('console.commands', function ($commands) use ($container) {
+                    $commands[] = new CreateDatabaseDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new DropDatabaseDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new CreateSchemaDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new UpdateSchemaDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new DropSchemaDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new RunDqlDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new RunSqlDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new ConvertMappingDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new ClearMetadataCacheDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new ClearQueryCacheDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new ClearResultCacheDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new InfoDoctrineCommand(null, $container['doctrine']);
+                    $commands[] = new ValidateSchemaCommand(null, $container['doctrine']);
+                    $commands[] = new EnsureProductionSettingsDoctrineCommand(null, $container['doctrine']);
+
+                    return $commands;
                 })
             );
         }
